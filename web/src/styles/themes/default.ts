e21ee0df334891721d951fd2fbed0737a7bb4035
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-09-16 17:57:07 -0300

    feat: add default app style

---
+++ web/src/styles/themes/default.ts
@@ -0,0 +1,14 @@
+export const defaultTheme = {
+  colors: {
+    white: '#FFFFFF',
+
+    'gray.100': '#DFDFDF',
+    'gray.500': '#8E8E8E',
+    'gray.600': '#4B4B4B',
+    'gray.900': '#121214',
+
+    'blue.500': '#004B76',
+
+    'yellow.500': '#FFD107',
+  },
+}
